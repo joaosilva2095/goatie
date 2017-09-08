66312eaa9bf78e4d478004d08b3d72a93e0168dd
João Silva <joaosilva@revtut.net>
2017-09-08 10:04:53 +0100

    Added JQuery dependency. Canvas is auto-resizing.

--- js/script.js
+++ js/script.js
@@ -3,14 +3,23 @@ var canvas,
     canvasHeight;
 
 function onLoad() {
+    console.log('Started loading');
     canvas = document.getElementById("board");
+
+    resizeCanvas();
+
+    console.log('Finished loading');
+}
+
+function resizeCanvas() {
+    console.log('Resizing the canvas');
+
     canvas.width = document.body.clientWidth;
     canvas.height = document.body.clientHeight;
     canvasWidth = canvas.width;
     canvasHeight = canvas.height;
-
-    if (canvas.getContext) {
-        setup();
-        setInterval(run, 33);
-    }
 }
+
+$(document).ready(onLoad);
+
+$(window).resize(resizeCanvas);
